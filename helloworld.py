8b8d9958e30e2ea83f049308a9ef6875734cd86e
zqli <457364723@qq.com>
2018-02-02 19:26:07 +0800

    print with %

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,13 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 # print "hello world!"
 # print "100 + 200 + 300 =", 100 + 200 + 300
 #
+# name = raw_input("please input a name: ")
+# print "hello, ", name
 
-name = raw_input("please input a name: ")
-print "hello, ", name
-
+print "hello, %s! age is %d" %("zqli", 27)
 
 
 
